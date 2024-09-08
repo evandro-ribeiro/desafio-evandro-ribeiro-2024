8ed2cb53ec2641c43b14c4632a88bebe3d79dbac
evandro-ribeiro <evandrormf.dev@gmail.com>
2024-09-08 13:26:34 -0300

    atualização do index para documentação

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import { RecintosZoo } from "./src/recintos-zoo.js";
 
+/**
+ * Teste de Console para validação da aplicação em desenvolvimento
+ */
+
 //VALIDAÇÃO INICIAL
-// new RecintosZoo().analisaRecintos("MACACO", 2);
-// new RecintosZoo().analisaRecintos("LEAO", 1);
+new RecintosZoo().analisaRecintos("MACACO", 2);
+new RecintosZoo().analisaRecintos("LEAO", 1);
 
 //VALIDAÇÃO DO LEOPARDO
-// new RecintosZoo().analisaRecintos("LEOPARDO", 1);
+new RecintosZoo().analisaRecintos("LEOPARDO", 1);
 
 //VALIDAÇÃO DO MACACO QUE NÃO PODE FICAR SOZINHO
-// new RecintosZoo().analisaRecintos("MACACO", 1);
+new RecintosZoo().analisaRecintos("MACACO", 1);
 
 //VALIDAÇÃO DO HIPOPOTAMO
-// new RecintosZoo().analisaRecintos("CROCODILO", 1);
-// new RecintosZoo().analisaRecintos("HIPOPOTAMO", 1);
+new RecintosZoo().analisaRecintos("CROCODILO", 1);
+new RecintosZoo().analisaRecintos("HIPOPOTAMO", 1);
 
 //VALIDAÇÕES DE ERROS
-// new RecintosZoo().analisaRecintos("MACACO", -2);
-// new RecintosZoo().analisaRecintos("UNICORNIO", 1);
+new RecintosZoo().analisaRecintos("MACACO", -2);
+new RecintosZoo().analisaRecintos("UNICORNIO", 1);
